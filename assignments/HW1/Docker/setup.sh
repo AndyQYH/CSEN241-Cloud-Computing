e3ca60c01ec42dbd2a411d660b6cbd9f5c9ce1f4
AndyQYH <qinandy2001@gmail.com>
2024-02-03 14:20:59 -0800

    updated docker setup script

--- assignments/HW1/Docker/setup.sh
+++ assignments/HW1/Docker/setup.sh
@@ -1,3 +1,4 @@
+<<com
 # This line uninstall thrid-party docker distribution
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 
@@ -21,3 +22,22 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin 
 
 # This Line is to test if Docker is setup correctly
 sudo docker run hello-world
+
+# Pull Original Ubuntu 20.04 Image From Docker Hub
+sudo docker pull amd64/ubuntu:20.04
+com
+sudo docker container prune --force
+# Run the base image with a given container name
+sudo docker run --name csen241-hw1-container -it -d --rm amd64/ubuntu:20.04
+# Update and setup Docker Environment for testing
+sudo docker exec -it csen241-hw1-container sh -c "apt update && apt upgrade -y"
+sudo docker exec -it csen241-hw1-container sh -c "apt install git sysbench -y"
+
+# Save update to a Docker Image
+sudo docker ps
+sudo docker commit csen241-hw1-container csen241-hw1-practice
+
+# Check and run Docker Image we just created
+sudo docker images
+sudo docker container rm csen241-hw1-container --force
+sudo docker run -it csen241-hw1-practice
